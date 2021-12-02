95cd6d3137e1a678dceb47ccbc5d731b7ea9c681
fhrii <fahri.ahmad@live.com>
2021-12-02 21:03:38 +0700

    return domain DetailThread as value

--- src/Applications/use_case/GetThreadUseCase.js
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -1,3 +1,7 @@
+import DetailComment from '../../Domains/comments/entities/DetailComment';
+import DetailReply from '../../Domains/replies/entities/DetailReply';
+import DetailThread from '../../Domains/threads/entities/DetailThread';
+
 class GetThreadUseCase {
   constructor({ threadRepository, commentRepository, replyRepository }) {
     this._threadRepository = threadRepository;
@@ -8,22 +12,51 @@ class GetThreadUseCase {
   async execute(useCasePayload) {
     this._verifyPayload(useCasePayload);
     const { id } = useCasePayload;
-    const [thread, comments] = await Promise.all([
-      this._threadRepository.getThreadById(id),
-      this._commentRepository.getCommentsByThreadId(id),
-    ]);
+    const [{ created_at: threadDate, ...thread }, comments] = await Promise.all(
+      [
+        this._threadRepository.getThreadById(id),
+        this._commentRepository.getCommentsByThreadId(id),
+      ]
+    );
 
+    thread.date = threadDate;
     thread.comments = await Promise.all(
       comments.map(async (comment) => {
+        const {
+          created_at: commentDate,
+          content: commentContent,
+          is_deleted: isCommentDeleted,
+          ...newComment
+        } = comment;
         const replies = await this._replyRepository.getRepliesByCommentId(
-          comment.id
+          newComment.id
         );
 
-        return { ...comment, replies };
+        newComment.content = !isCommentDeleted
+          ? commentContent
+          : '**komentar telah dihapus**';
+        newComment.date = commentDate;
+        newComment.replies = replies.map(
+          ({
+            created_at: replyDate,
+            content: replyContent,
+            is_deleted: isReplyDeleted,
+            ...reply
+          }) =>
+            new DetailReply({
+              ...reply,
+              content: !isReplyDeleted
+                ? replyContent
+                : '**balasan telah dihapus**',
+              date: replyDate,
+            })
+        );
+
+        return new DetailComment({ ...newComment, date: commentDate });
       })
     );
 
-    return thread;
+    return new DetailThread(thread);
   }
 
   _verifyPayload({ id }) {
--- src/Applications/use_case/_test/GetThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadUseCase.test.js
@@ -1,5 +1,8 @@
 import CommentRepository from '../../../Domains/comments/CommentRepository';
+import DetailComment from '../../../Domains/comments/entities/DetailComment';
+import DetailReply from '../../../Domains/replies/entities/DetailReply';
 import ReplyRepository from '../../../Domains/replies/ReplyRepository';
+import DetailThread from '../../../Domains/threads/entities/DetailThread';
 import ThreadRepository from '../../../Domains/threads/ThreadRepository';
 import GetThreadUseCase from '../GetThreadUseCase';
 
@@ -34,47 +37,96 @@ describe('GetThreadUseCase', () => {
       id: 'thread-123',
     };
     const definedDate = new Date().toISOString();
-    const expectingThread = {
+    const expectedThread = {
       id: 'thread-123',
       title: 'some title',
       body: 'some body',
       username: 'dicoding',
-      date: definedDate,
+      created_at: definedDate,
     };
-    const expectingComments = [
+    const expectedComments = [
       {
         id: 'comment-123',
         username: 'dicoding',
-        date: definedDate,
+        is_deleted: false,
+        created_at: definedDate,
         content: 'some comment content',
       },
+      {
+        id: 'comment-234',
+        username: 'dicoding',
+        is_deleted: true,
+        created_at: definedDate,
+        content: 'some comment2 content',
+      },
     ];
-    const expectingReplies = [
+    const expectedReplies = [
       {
         id: 'reply-123',
         username: 'dicoding',
-        date: definedDate,
+        is_deleted: true,
+        created_at: definedDate,
         content: 'some reply content',
       },
+      {
+        id: 'reply-234',
+        username: 'dicoding',
+        is_deleted: false,
+        created_at: definedDate,
+        content: 'some reply2 content',
+      },
     ];
-    const expectingDetailThread = {
-      ...expectingThread,
-      comments: [{ ...expectingComments[0], replies: expectingReplies }],
-    };
+    const expectedDetailThread = new DetailThread({
+      id: expectedThread.id,
+      title: expectedThread.title,
+      body: expectedThread.body,
+      username: expectedThread.username,
+      date: expectedThread.created_at,
+      comments: expectedComments.map(
+        ({
+          created_at: commentDate,
+          content: commentContent,
+          is_deleted: isCommentDeleted,
+          ...comment
+        }) =>
+          new DetailComment({
+            ...comment,
+            content: !isCommentDeleted
+              ? commentContent
+              : '**komentar telah dihapus**',
+            date: commentDate,
+            replies: expectedReplies.map(
+              ({
+                created_at: replyDate,
+                content: replyContent,
+                is_deleted: isReplyDeleted,
+                ...reply
+              }) =>
+                new DetailReply({
+                  ...reply,
+                  content: !isReplyDeleted
+                    ? replyContent
+                    : '**balasan telah dihapus**',
+                  date: replyDate,
+                })
+            ),
+          })
+      ),
+    });
     const mockThreadRepository = new ThreadRepository();
     const mockCommentRepository = new CommentRepository();
     const mockReplyRepository = new ReplyRepository();
 
     // Mocking
-    mockThreadRepository.getThreadById = jest
-      .fn()
-      .mockImplementation(() => Promise.resolve(expectingThread));
-    mockCommentRepository.getCommentsByThreadId = jest
-      .fn()
-      .mockImplementation(() => Promise.resolve(expectingComments));
-    mockReplyRepository.getRepliesByCommentId = jest
-      .fn()
-      .mockImplementation(() => Promise.resolve(expectingReplies));
+    mockThreadRepository.getThreadById = jest.fn(() =>
+      Promise.resolve(expectedThread)
+    );
+    mockCommentRepository.getCommentsByThreadId = jest.fn(() =>
+      Promise.resolve(expectedComments)
+    );
+    mockReplyRepository.getRepliesByCommentId = jest.fn(() =>
+      Promise.resolve(expectedReplies)
+    );
 
     // Action
     const getThreadUseCase = new GetThreadUseCase({
@@ -85,7 +137,7 @@ describe('GetThreadUseCase', () => {
     const thread = await getThreadUseCase.execute(useCasePayload);
 
     // Assert
-    expect(thread).toStrictEqual(expectingDetailThread);
+    expect(thread).toStrictEqual(expectedDetailThread);
     expect(mockThreadRepository.getThreadById).toBeCalledWith(
       useCasePayload.id
     );
@@ -93,7 +145,10 @@ describe('GetThreadUseCase', () => {
       useCasePayload.id
     );
     expect(mockReplyRepository.getRepliesByCommentId).toBeCalledWith(
-      expectingComments[0].id
+      expectedComments[0].id
+    );
+    expect(mockReplyRepository.getRepliesByCommentId).toBeCalledWith(
+      expectedComments[1].id
     );
   });
 });
