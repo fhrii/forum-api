67feb9df97a437db41ebeecb9a67713cdf60f03c
fhrii <fahri.ahmad@live.com>
2021-12-08 16:10:56 +0700

    add like count property

--- src/Domains/comments/entities/DetailComment.js
+++ src/Domains/comments/entities/DetailComment.js
@@ -4,17 +4,25 @@ class DetailComment {
   constructor(payload) {
     this._verifyPayload(payload);
 
-    const { id, content, date, username, replies } = payload;
+    const { id, content, date, username, likeCount, replies } = payload;
 
     this.id = id;
     this.content = content;
     this.date = date;
     this.username = username;
+    this.likeCount = likeCount;
     this.replies = replies;
   }
 
-  _verifyPayload({ id, content, date, username, replies }) {
-    if (!id || !content || !date || !username || !replies)
+  _verifyPayload({ id, content, date, username, likeCount, replies }) {
+    if (
+      !id ||
+      !content ||
+      !date ||
+      !username ||
+      typeof likeCount === 'undefined' ||
+      !replies
+    )
       throw new Error('DETAIL_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
 
     if (
@@ -22,6 +30,7 @@ class DetailComment {
       typeof content !== 'string' ||
       typeof date !== 'string' ||
       typeof username !== 'string' ||
+      typeof likeCount !== 'number' ||
       !Array.isArray(replies) ||
       replies.some((reply) => !(reply instanceof DetailReply))
     )
